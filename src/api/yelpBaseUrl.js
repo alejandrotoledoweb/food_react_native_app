653f927f8a91d50438f8b1dafda0ef3c1f52f66d
Alejandro Toledo <atoledofr@gmail.com>
2023-09-21 14:46:24 -0500

    delete api key

--- src/api/yelpBaseUrl.js
+++ src/api/yelpBaseUrl.js
@@ -3,8 +3,9 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    // Authorization: "Bearer " + process.env.API_KEY,
-    Authorization:
-      "Bearer 0ruQM8Sn4_6ZGzh_p17BnDRyu3TOFyrNlLg-XO6EGqhf0-_KjcuOpzRuehPB6nP11VYiiTXY7CMjSWmMndbGvqkmyEAXRdgirXobfH-ChlzwPwGvfjgKcKhKo08LZXYx",
+    Authorization: "Bearer " + process.env.API_KEY,
   },
+  //   Authorization:
+  //     "Bearer 0ruQM8Sn4_6ZGzh_p17BnDRyu3TOFyrNlLg-XO6EGqhf0-_KjcuOpzRuehPB6nP11VYiiTXY7CMjSWmMndbGvqkmyEAXRdgirXobfH-ChlzwPwGvfjgKcKhKo08LZXYx",
+  // },
 });
